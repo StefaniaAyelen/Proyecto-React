1d1fc8f06c414f72b9e85412ce77150a88812b7c
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-10-21 17:28:14 -0300

    Interfaz del carrito

--- src/components/carrito.jsx
+++ src/components/carrito.jsx
@@ -1,1 +1,15 @@
 // Muestra los productos que el usuario agrego
+const Carrito = ({carrito}) => {
+    return(
+        <div>
+            {carrito.length === 0 
+            ? <h3>No hay productos todavia</h3>
+            : carrito.map(producto => (
+                <p key={producto.id}>{producto.title} - ${producto.price}</p>
+            ))                
+            }
+        </div>
+    )
+}
+
+export default Carrito;
