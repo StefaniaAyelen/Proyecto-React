0591314046c718d24f450008c2a5efbca6ba0c1d
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-10-22 15:25:40 -0300

    Renderizo la pagina

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,12 @@
 import ListaProductos from "./ListaProductos";
 import Carrito from "./carrito";
 
-function Main({carrito , agregarAlCarrito}) {
+function Main({carrito , agregarAlCarrito, vaciarCarrito}) {
     return (
         <main>
         <div>
             <ListaProductos agregarAlCarrito={agregarAlCarrito}></ListaProductos>
-            <Carrito carrito={carrito}></Carrito>
+            <Carrito carrito={carrito} vaciarCarrito={vaciarCarrito}></Carrito>
         </div>
         </main>
     );
