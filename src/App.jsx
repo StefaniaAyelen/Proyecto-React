1b79a891df7fc88ef0c7214483f7477f0ca41d52
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-10-22 15:25:02 -0300

    Agregue rutas estaticas

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header';
 import Navbar from './components/Navbar';
-import Main from './components/Main';
 import Footer from './components/Footer';
+import Inicio from './pages/Inicio';
+import Moda from './pages/Moda';
+import PagCarrito from './pages/PagCarrito';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
   const [carrito, setCarrito] = useState([]);
@@ -12,12 +15,24 @@ function App() {
     setCarrito([...carrito, producto])
   }
 
+  function vaciarCarrito() {
+    setCarrito([])
+  }
+
   return (
     <div className="min-h-screen bg-gray-50 text-gray-800 font-sans flex flex-col">
-      <Header />
-      <Navbar />
-      <Main agregarAlCarrito={agregarAlCarrito} carrito={carrito} />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Navbar />
+          <div>
+            <Routes>
+              <Route path="/" element={<Inicio agregarAlCarrito={agregarAlCarrito} carrito={carrito} vaciarCarrito={vaciarCarrito} />} />
+              <Route path="/Moda" element={<Moda />} />
+              <Route path='/pagCarrito' element={<PagCarrito/>}/>
+          </Routes>
+          </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
