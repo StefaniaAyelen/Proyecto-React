2550a578ea7fa42c1e98b3a47a77b37cc9360ea2
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-10-22 15:27:31 -0300

    Estile el carrito y agregue el boton vaciar

--- src/components/carrito.jsx
+++ src/components/carrito.jsx
@@ -1,13 +1,18 @@
 // Muestra los productos que el usuario agrego
-const Carrito = ({carrito}) => {
+const Carrito = ({carrito, vaciarCarrito}) => {
     return(
         <div>
             {carrito.length === 0 
             ? <h3>No hay productos todavia</h3>
             : carrito.map(producto => (
                 <p key={producto.id}>{producto.title} - ${producto.price}</p>
-            ))                
-            }
+            ))}
+            <button
+                onClick={() => vaciarCarrito()}
+                className="mt-4 bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700 transition w-full font-medium cursor-pointer"
+            >
+                VaciarCarrito
+            </button>          
         </div>
     )
 }
