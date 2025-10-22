13a2d03a2ff40f5171376a6b28ec5499a89148e3
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-10-22 15:25:53 -0300

    Agregue los link to

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 // Estructura visual del Navbar
+
+import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <nav className="bg-indigo-700 text-white flex justify-center gap-8 py-3 shadow">
-        <a href="#" className="hover:text-yellow-300 transition font-medium">
-            Inicio
-        </a>
-        <a href="#" className="hover:text-yellow-300 transition font-medium">
-            Moda
-        </a>
-        <a href="#" className="hover:text-yellow-300 transition font-medium">
-            Carrito
-        </a>
+            <Link to="/" className="hover:text-yellow-300 transition font-medium">
+                Inicio
+            </Link>
+            <Link to="/moda" className="hover:text-yellow-300 transition font-medium">
+                Moda
+            </Link>
+            <Link to="/PagCarrito" className="hover:text-yellow-300 transition font-medium">
+                Carrito
+            </Link>
         </nav>
     );
 }
