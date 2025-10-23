3f5098e244c50db4d620c4d95c61c12d470e143b
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-10-22 22:39:27 -0300

    Agregue contador de productos en el carrito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 // Estructura visual del Header
-function Header() {
+function Header({carrito}) {
     return (
         <header className="bg-indigo-600 text-white py-4 px-8 shadow-md flex justify-between items-center">
         <h1 className="text-2xl font-bold tracking-wide">Mi Tienda</h1>
         <span className="bg-white text-indigo-600 px-3 py-1 rounded-full font-semibold">
-            🛒 0
+            🛒 {carrito.length}
         </span>
         </header>
     );
