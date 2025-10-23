aead90c6baf4ae35d6c744dc6fbb7545fe71483f
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-10-23 01:20:23 -0300

    Agregue el nombre de Urbanita

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer() {
     return (
         <footer className="bg-gray-800 text-white text-center py-4 mt-12">
-        <p>© 2025 Mi Tienda — Todos los derechos reservados.</p>
+        <p>© 2025 Urbanita — Todos los derechos reservados.</p>
         </footer>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 function Header({carrito}) {
     return (
         <header className="bg-indigo-600 text-white py-4 px-8 shadow-md flex justify-between items-center">
-        <h1 className="text-2xl font-bold tracking-wide">Mi Tienda</h1>
+        <h1 className="text-2xl font-bold tracking-wide">Urbanita Store</h1>
         <span className="bg-white text-indigo-600 px-3 py-1 rounded-full font-semibold">
             🛒 {carrito.length}
         </span>
