8a60dfa9f9bf415d20da424aca19afdeed055e71
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-12-02 22:02:33 -0300

    Add context

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,39 @@
-import { useState } from 'react'
+import CarritoProvider from './context/CarritoContext';
 import './App.css'
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Inicio from './pages/Inicio';
 import Moda from './pages/Moda';
+import Login from './pages/Login'
 import PagCarrito from './pages/PagCarrito';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import DetalleProducto from './pages/DetalleProducto';
+import ProtectedRoute from './components/ProtectedRoute';
+import AuthProvider from './context/AuthContext';
 
 function App() {
-  const [carrito, setCarrito] = useState([]);
-
-  function agregarAlCarrito(producto) {
-    setCarrito([...carrito, producto])
-  }
-
-  function vaciarCarrito() {
-    setCarrito([])
-  }
-
-  function eliminarDelCarrito(id) {
-    const nuevoCarrito = carrito.filter(producto => producto.id !== id);
-    setCarrito(nuevoCarrito);
-  }
-
 
   return (
     <div className="min-h-screen bg-gray-50 text-gray-800 font-sans flex flex-col">
-      <BrowserRouter>
-        <Header carrito={carrito} />
-        <Navbar />
-          <div className='flex-1'>
-            <Routes>
-              <Route path="/" element={<Inicio agregarAlCarrito={agregarAlCarrito} />} />
-              <Route path="/Moda" element={<Moda agregarAlCarrito={agregarAlCarrito} />} />
-              <Route path='/pagCarrito' element={<PagCarrito carrito={carrito} vaciarCarrito={vaciarCarrito} eliminarDelCarrito={eliminarDelCarrito}/>}/>
-              <Route path="/producto/:id" element={<DetalleProducto />} />
-          </Routes>
-          </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <CarritoProvider>
+          <BrowserRouter>
+            <Header/>
+            <Navbar />
+              <div className='flex-1'>
+                <Routes>
+                  <Route path="/login" element={<Login/>} />
+                  <Route path="/" element={<Inicio/>} />
+                  <Route path="/Moda" element={<Moda/>} />
+                  <Route path='/pagCarrito' element={<ProtectedRoute> <PagCarrito/> </ProtectedRoute> }/>
+                  <Route path="/producto/:id" element={<DetalleProducto/>} />
+              </Routes>
+              </div>
+            <Footer />
+          </BrowserRouter>
+        </CarritoProvider>
+      </AuthProvider>
     </div>
   );
 }
