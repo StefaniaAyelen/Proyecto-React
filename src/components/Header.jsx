5cc12a4ffdec75e469b4335ef69dd328b55e94c3
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-12-02 22:03:58 -0300

    Implements context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,37 @@
 // Estructura visual del Header
-function Header({carrito}) {
+import { useContext } from "react";
+import { CarritoContext } from "../context/CarritoContext";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+function Header() {
+
+    const { carrito } = useContext(CarritoContext);
+    const cantidadTotal = carrito.length;
+    const navigate = useNavigate();
+    const { isLoggedIn, logout } = useContext(AuthContext);
+    const handleAuthClick = () => {
+        if (isLoggedIn) {
+            // Si está logueado, al hacer clic, hace LOGOUT
+            logout();
+        } else {
+            // Si NO está logueado, al hacer clic, va a LOGIN
+            navigate('/login');
+        }
+    };
+    
     return (
         <header className="bg-indigo-600 text-white py-4 px-8 shadow-md flex justify-between items-center">
         <h1 className="text-2xl font-bold tracking-wide">Urbanita Store</h1>
         <span className="bg-white text-indigo-600 px-3 py-1 rounded-full font-semibold">
-            🛒 {carrito.length}
+            🛒 {cantidadTotal}
         </span>
+        <button 
+                    onClick={handleAuthClick}
+                    className="bg-white text-black bg-opacity-20 hover:bg-opacity-30 px-3 py-1 rounded-md font-medium transition"
+                >
+                    {isLoggedIn ? 'Cerrar Sesión' : 'Iniciar Sesión'}
+                </button>
         </header>
     );
 }
